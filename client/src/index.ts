485eb09f0371863b43a29186e04adca81c8af4af
Tenjou <arthur.der.shefer@gmail.com>
2020-10-12 01:41:46 +0300

    add: character rendering & input handling

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,5 +1,25 @@
+const characterSize = 32
+const characterSpeed = 100
+const pressedInput: Record<string, boolean> = {}
 let canvas: HTMLCanvasElement = null
 let ctx: CanvasRenderingContext2D = null
+let tPrev: number = 0
+let player: Character = null
+
+class Vector2 {
+    x: number = 0
+    y: number = 0
+
+    set(x: number, y: number) {
+        this.x = x
+        this.y = y
+    }
+}
+
+class Character {
+    position = new Vector2()
+    speed = new Vector2()
+}
 
 const create = () => {
     canvas = document.createElement("canvas")
@@ -9,8 +29,15 @@ const create = () => {
     handleResize()
 
     window.addEventListener("resize", handleResize)
-    window.addEventListener("onkeydown", handleKeyDown)
-    window.addEventListener("onkeyup", handleKeyUp)
+    window.addEventListener("keydown", handleKeyDown)
+    window.addEventListener("keyup", handleKeyUp)
+
+    tPrev = Date.now()
+}
+
+const setup = () => {
+    player = new Character()
+    player.position.set(100, 100)
 }
 
 const handleResize = () => {
@@ -18,16 +45,61 @@ const handleResize = () => {
     canvas.height = window.innerHeight
 }
 
-const handleKeyDown = (event: UIEvent) => {}
-
-const handleKeyUp = (event: UIEvent) => {}
-
-const render = () => {
-    ctx.fillStyle = "#ccc"
-    ctx.fillRect(0, 0, canvas.width, canvas.height)
+const handleKeyDown = (event: KeyboardEvent) => {
+    pressedInput[event.code] = true
 }
 
-const renderPlayer = (x: number, y: number) => {}
+const handleKeyUp = (event: KeyboardEvent) => {
+    pressedInput[event.code] = false
+}
+
+const update = () => {
+    const tNow = Date.now()
+    const tDeltaF = (tNow - tPrev) / 1000
+
+    if (pressedInput["ArrowLeft"] || pressedInput["KeyA"]) {
+        player.speed.x = -characterSpeed
+    } else if (pressedInput["ArrowRight"] || pressedInput["KeyD"]) {
+        player.speed.x = characterSpeed
+    } else {
+        player.speed.x = 0
+    }
+
+    if (pressedInput["ArrowUp"] || pressedInput["KeyW"]) {
+        player.speed.y = -characterSpeed
+    } else if (pressedInput["ArrowDown"] || pressedInput["KeyS"]) {
+        player.speed.y = characterSpeed
+    } else {
+        player.speed.y = 0
+    }
+
+    player.position.x += player.speed.x * tDeltaF
+    player.position.y += player.speed.y * tDeltaF
+
+    tPrev = tNow
+}
+
+const render = () => {
+    update()
+
+    ctx.fillStyle = "#ccc"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    renderCharacter(player)
+
+    requestAnimationFrame(render)
+}
+
+const renderCharacter = (character: Character) => {
+    ctx.fillStyle = "red"
+    ctx.fillRect(
+        character.position.x,
+        character.position.y,
+        characterSize,
+        characterSize
+    )
+}
 
 create()
+setup()
 render()
