c869bb4bc167135cc4cf5be2b2bb56472ab6792f
Tenjou <arthur.der.shefer@gmail.com>
2020-10-21 23:39:06 +0300

    add: Vector3.mul

--- client/src/Vector2.ts
+++ client/src/Vector2.ts
@@ -1,9 +1,24 @@
 export default class Vector2 {
-    x: number = 0
-    y: number = 0
+    x: number
+    y: number
+
+    constructor(x: number = 0, y: number = 0) {
+        this.x = x
+        this.y = y
+    }
 
     set(x: number, y: number) {
         this.x = x
         this.y = y
+    }
+
+    mul(value: number) {
+        this.x *= value
+        this.y *= value
+    }
+
+    rotate(angleRad: number) {
+        this.x = Math.cos(angleRad)
+        this.y = Math.sin(angleRad)
     }
 }
